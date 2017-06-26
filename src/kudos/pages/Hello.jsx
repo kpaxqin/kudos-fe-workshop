a2ff21745637ae5eed75a40963212e1e35997060
Junwen Qin <jwqin@thoughtworks.com>
2017-06-26 14:17:15 +0800

    wrap Hello page with layout

--- src/kudos/pages/Hello.jsx
+++ src/kudos/pages/Hello.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { flowRight } from 'lodash';
+import { withRedux } from '../../shared/redux';
+import { layoutWrapper } from '../../shared/dashboard';
 
-const Hello = () => <div>Hello</div>;
+const Hello = () => (
+  <div>Hello</div>
+);
 
-export default Hello;
+const decorator = flowRight([
+  withRedux(),
+  layoutWrapper,
+]);
+
+export default decorator(Hello);
