8e014a754cad80bde7e50d667c9f04ab4cdae8a0
Junwen Qin <jwqin@thoughtworks.com>
2017-06-28 14:16:14 +0800

    remove unused code

--- src/account/actions/index.js
+++ src/account/actions/index.js
@@ -3,7 +3,6 @@ import { SubmissionError } from 'redux-form';
 
 import auth from '../../shared/api/auth';
 import userStorage from '../../shared/storage/user';
-import routes from '../../shared/constants/routes';
 import { push } from '../../shared/router';
 import actionTypes from '../constants/actionTypes';
 
@@ -21,18 +20,11 @@ const ensureUser = createAction(actionTypes.ENSURE_USER);
 const signOutAction = createAsyncAction(
   actionTypes.SIGN_OUT,
   (payload, dispatch) => userStorage.removeUser()
-    .then(() => dispatch(push(routes.SIGN_IN))),
-);
-
-const selectStoreAction = createAsyncAction(
-  actionTypes.SELECT_STORE,
-  (aStore, dispatch) => userStorage.setCurrentStore(aStore)
-    .then(() => dispatch(push(routes.DASHBOARD))),
+    .then(() => dispatch(push('/sign-in'))),
 );
 
 export default {
   signInAction,
   ensureUser,
   signOutAction,
-  selectStoreAction,
 };
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import routes from './shared/constants/routes';
 import Account from './account';
 import Kudos from './kudos';
 
 function getRoutes() {
   return (
     <Switch>
-      <Route exact path={routes.SIGN_IN} component={Account.Login} />
+      <Route exact path={'/sign-in'} component={Account.Login} />
       <Route exact path={'/hello'} component={Kudos.Hello} />
     </Switch>
   );
