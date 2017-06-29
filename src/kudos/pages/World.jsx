1d6e4c75be6773454b80469272d75ce2b92ce082
Junwen Qin <jwqin@thoughtworks.com>
2017-06-29 11:22:45 +0800

    add hoc for auth

--- src/kudos/pages/World.jsx
+++ src/kudos/pages/World.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { flowRight } from 'lodash';
 import { connect } from 'react-redux';
 import Dashboard from '../../shared/dashboard';
 import { withRedux } from '../../shared/redux';
+import userStorage from '../../shared/storage/user';
+
+const connectAuthCheck = checkFn => (Content) => {
+  class AuthCheck extends Component {
+    constructor() {
+      super();
+      this.state = {
+        authed: false,
+      };
+    }
+    componentWillMount() {
+      this.doCheck(this.props);
+    }
+    doCheck(props) {
+      Promise.resolve(checkFn(props))
+        .then(() => {
+          this.setState({ authed: true });
+        }, (e) => {
+          console.warn('Page you requested is not allowed: ', e);
+        });
+    }
+    render() {
+      const { authed } = this.state;
+      return (
+        authed ? <Content {...this.props} /> : null
+      );
+    }
+  }
+
+  return AuthCheck;
+};
+
 
 const World = ({ currentUser }) => <Dashboard><div>World of {currentUser.name}!</div></Dashboard>;
 
 const enhance = flowRight([
+  connectAuthCheck(() => userStorage.getUser()),
   withRedux(),
   connect(state => state),
 ]);
