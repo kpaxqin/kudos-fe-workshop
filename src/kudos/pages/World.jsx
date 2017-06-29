0b2f47114c8e6c083405d482fc82fefabd08cd45
Junwen Qin <jwqin@thoughtworks.com>
2017-06-29 10:57:43 +0800

    add redux integration

--- src/kudos/pages/World.jsx
+++ src/kudos/pages/World.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { withRedux } from '../../shared/redux';
 
-const World = () => <div>World!</div>;
+const World = ({ currentUser }) => <div>World of {currentUser.name}!</div>;
 
-export default World;
+export default withRedux()(connect(state => state)(World));
