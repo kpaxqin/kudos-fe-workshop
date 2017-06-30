2966e7c5d83a3ed3f0c6c181425b0847ab49f047
Junwen Qin <jwqin@thoughtworks.com>
2017-06-30 17:05:50 +0800

    request to api

--- src/kudos/pages/List.jsx
+++ src/kudos/pages/List.jsx
@@ -3,6 +3,7 @@ import { flowRight } from 'lodash';
 import { withRedux } from '../../shared/redux';
 import { layoutWrapper } from '../../shared/dashboard';
 import ListTable from '../components/ListTable';
+import listApi from '../../shared/api/list';
 
 const columns = [
   {
@@ -16,83 +17,37 @@ const columns = [
   },
 ];
 
-const datas = {
-  totalCount: 49,
-  pageSize: 10,
-  pageIndex: 1,
-  items: [
-    {
-      id: '0ec8a9b6-2640-4ca4-87e3-a25d80fb2a4d',
-      name: 'asdf Lazaro Schmeler',
-      role: 'ADMIN',
-      address: '925 Nina Park',
-    },
-    {
-      id: '53ce571c-deff-4545-bb3d-9675cdf9d503',
-      name: 'asdf Clement Upton',
-      role: 'ADMIN',
-      address: '6910 Jast Vista',
-    },
-    {
-      id: '21f67014-51df-4ed7-85c5-e94dbd36d61e',
-      name: 'asdf Mary Bernier',
-      role: 'ADMIN',
-      address: '563 Nikita Drive',
-    },
-    {
-      id: '1f128279-aafe-463a-aa2b-52dc1b7c6c5b',
-      name: 'asdf Camron DuBuque',
-      role: 'ADMIN',
-      address: '860 Bernier Way',
-    },
-    {
-      id: 'd9add5ff-b0c5-48df-a4df-725239134016',
-      name: 'asdf Jerome Hagenes',
-      role: 'ADMIN',
-      address: '200 Mayert Brook',
-    },
-    {
-      id: 'fa80fd02-ebef-4d5f-b2ee-81dfaba72d6d',
-      name: 'asdf Victor Borer',
-      role: 'ADMIN',
-      address: '034 Jessyca Forges',
-    },
-    {
-      id: '438d91db-0f5b-4096-9064-c267d34b88b0',
-      name: 'asdf Ephraim Torp',
-      role: 'ADMIN',
-      address: '21709 Antonina Streets',
-    },
-    {
-      id: '33531c36-030e-44e7-9bc1-7f63182b4c70',
-      name: 'asdf Ollie Sawayn',
-      role: 'ADMIN',
-      address: '732 Lonie Tunnel',
-    },
-    {
-      id: '22d3080c-dfc3-4b25-83b4-7656ca72a20b',
-      name: 'asdf Asa Mertz',
-      role: 'ADMIN',
-      address: '881 McLaughlin Walk',
-    },
-    {
-      id: 'ce296d98-cd66-4879-8f95-058f8917e847',
-      name: 'asdf Sigmund Schowalter',
-      role: 'ADMIN',
-      address: '5222 Luettgen Overpass',
-    },
-  ],
-};
-
 class List extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      list: {
+        totalCount: 0,
+        pageIndex: 1,
+        pageSize: 10,
+        items: [],
+      },
+    };
+  }
+  componentDidMount() {
+    this.getList(1);
+  }
+  getList(pageIndex, query) {
+    listApi.getList({
+      ...query,
+      pageIndex,
+    }).then(list => this.setState({ list }));
+  }
   handleSelectPage = (nextPageIndex) => {
-    console.log(nextPageIndex);
+    this.getList(nextPageIndex);
   }
   render() {
+    const { list } = this.state;
     return (
       <ListTable
         columns={columns}
-        list={datas}
+        list={list}
         onSelectPage={this.handleSelectPage}
       />
     );
