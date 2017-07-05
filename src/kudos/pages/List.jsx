bd774d8a7f82781e7539671e0d1f2624fd869193
Junwen Qin <jwqin@thoughtworks.com>
2017-07-05 23:16:35 +0800

    extract connectListPage HOC

--- src/kudos/pages/List.jsx
+++ src/kudos/pages/List.jsx
@@ -9,6 +9,72 @@ import listApi from '../../shared/api/list';
 import makePath from '../../shared/utils/makePath';
 import history from '../../history';
 
+
+const connectListPage = ({ getListData }) => (Page) => {
+  class ListPage extends Component {
+    constructor() {
+      super();
+
+      this.state = {
+        list: {
+          totalCount: 0,
+          pageIndex: 1,
+          pageSize: 10,
+          items: [],
+        },
+      };
+    }
+    componentDidMount() {
+      this.getList(this.props.location);
+    }
+    componentWillReceiveProps(nextProps) {
+      this.getList(nextProps.location);
+    }
+    getList(location) {
+      const { pageIndex = 1, ...searchParams } = parse(location.search);
+
+      getListData({
+        ...searchParams,
+        pageIndex,
+      }).then(list => this.setState({ list }));
+    }
+    changeUrlQuery(nextQuery, isOverride) {
+      const { location: { pathname, search } } = this.props;
+      const currentQuery = parse(search);
+      const nextPath = makePath(
+        pathname,
+        undefined,
+        isOverride ? nextQuery : { ...currentQuery, ...nextQuery },
+      );
+      history.push(nextPath);
+    }
+    handleSelectPage = (nextPageIndex) => {
+      this.changeUrlQuery({
+        pageIndex: nextPageIndex,
+      });
+    }
+    handleSearch = (search) => {
+      this.changeUrlQuery({
+        ...search,
+        pageIndex: 1,
+      }, true);
+    }
+    render() {
+      const { list } = this.state;
+      const { pageIndex, ...searchParams } = parse(this.props.location.search);
+      return (
+        <Page
+          list={list}
+          searchParams={searchParams}
+          searchListPage={this.handleSearch}
+          selectPage={this.handleSelectPage}
+        />
+      );
+    }
+  }
+  return ListPage;
+};
+
 const columns = [
   {
     key: 'name',
@@ -21,74 +87,26 @@ const columns = [
   },
 ];
 
-
-class List extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      list: {
-        totalCount: 0,
-        pageIndex: 1,
-        pageSize: 10,
-        items: [],
-      },
-    };
-  }
-  componentDidMount() {
-    this.getList(this.props.location);
-  }
-  componentWillReceiveProps(nextProps) {
-    this.getList(nextProps.location);
-  }
-  getList(location) {
-    const { pageIndex = 1, ...searchParams } = parse(location.search);
-
-    listApi.getList({
-      ...searchParams,
-      pageIndex,
-    }).then(list => this.setState({ list }));
-  }
-  changeUrlQuery(nextQuery, isOverride) {
-    const { location: { pathname, search } } = this.props;
-    const currentQuery = parse(search);
-    const nextPath = makePath(
-      pathname,
-      undefined,
-      isOverride ? nextQuery : { ...currentQuery, ...nextQuery },
-    );
-    history.push(nextPath);
-  }
-  handleSelectPage = (nextPageIndex) => {
-    this.changeUrlQuery({
-      pageIndex: nextPageIndex,
-    });
-  }
-  handleSearch = (search) => {
-    this.changeUrlQuery({
-      ...search,
-      pageIndex: 1,
-    }, true);
-  }
-  render() {
-    const { list } = this.state;
-    const { pageIndex, ...searchParams } = parse(this.props.location.search);
-    return (
-      <div className="content">
-        <SearchBar searchParams={searchParams} onSearch={this.handleSearch} />
-        <ListTable
-          columns={columns}
-          list={list}
-          onSelectPage={this.handleSelectPage}
-        />
-      </div>
-    );
-  }
-}
+const List = (props) => {
+  const { list, searchParams, searchListPage, selectPage } = props;
+  return (
+    <div className="content">
+      <SearchBar searchParams={searchParams} onSearch={searchListPage} />
+      <ListTable
+        columns={columns}
+        list={list}
+        onSelectPage={selectPage}
+      />
+    </div>
+  );
+};
 
 const decorator = flowRight([
   withRedux(),
   layoutWrapper,
+  connectListPage({
+    getListData: listApi.getList,
+  }),
   // connect(state => state),
 ]);
 
