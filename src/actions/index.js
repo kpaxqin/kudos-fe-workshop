353f6ee644502d6493bbabe92f1c502094336c41
Junwen Qin <jwqin@thoughtworks.com>
2017-07-07 15:58:41 +0800

    move error handle to reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,17 +2,15 @@ import * as types from '../constants/ActionTypes'
 import api from '../api';
 
 const addTodoSuccess = text => ({ type: types.ADD_TODO, text })
-
-const errorHandler = e => {
-  window.alert(e)
-  throw e;
-};
+const addTodoFailed = e => ({ type: types.ADD_TODO_FAILED, payload: e, error: true })
 
 export const addTodo = text => dispatch => Promise.all([api.addTodo(text), api.addTodo(text)])
   .then(([data])=> {
     dispatch(addTodoSuccess(text));
-  }, errorHandler);
-  
+  }, (e)=> {
+    dispatch(addTodoFailed(e));
+  });
+
 export const deleteTodo = id => ({ type: types.DELETE_TODO, id })
 export const editTodo = (id, text) => ({ type: types.EDIT_TODO, id, text })
 export const completeTodo = id => ({ type: types.COMPLETE_TODO, id })
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,18 @@
 import { combineReducers } from 'redux'
 import todos from './todos'
+import * as types from '../constants/ActionTypes'
 
 const rootReducer = combineReducers({
-  todos
+  todos,
+  error(state = null, action) {
+    switch(action.type) {
+      case types.ADD_TODO_FAILED:
+        // shouldn't call alert in reducer, just for demostrate
+        window.alert(action.payload); 
+        return action.payload;
+      default: return state;
+    }
+  }
 })
 
 export default rootReducer
