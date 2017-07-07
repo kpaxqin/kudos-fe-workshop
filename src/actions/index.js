c8168f24a378b460c15a6c8672b9cac1499fc999
Junwen Qin <jwqin@thoughtworks.com>
2017-07-07 15:00:28 +0800

    deal breaker for api level handler

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,9 +3,10 @@ import api from '../api';
 
 const addTodoSuccess = text => ({ type: types.ADD_TODO, text })
 
-export const addTodo = text => dispatch => api.addTodo(text).then((data)=> {
-  dispatch(addTodoSuccess(text));
-});
+export const addTodo = text => dispatch => Promise.all([api.addTodo(text), api.addTodo(text)])
+  .then(([data])=> {
+    dispatch(addTodoSuccess(text));
+  });
 export const deleteTodo = id => ({ type: types.DELETE_TODO, id })
 export const editTodo = (id, text) => ({ type: types.EDIT_TODO, id, text })
 export const completeTodo = id => ({ type: types.COMPLETE_TODO, id })
